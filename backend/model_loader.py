b41787beca62da107d26fee5aec2aa9ea43a4a7c
Harman Aujla <harman11524@gmail.com>
2026-02-25 17:53:54 -0500

    Switched to smaller model for better runtime

--- backend/model_loader.py
+++ backend/model_loader.py
@@ -1,6 +1,6 @@
 import torch
 from vjepa2.src.models.attentive_pooler import AttentiveClassifier
-from vjepa2.src.models.vision_transformer import vit_giant_xformers_rope
+from vjepa2.src.models.vision_transformer import vit_large_rope
 from backend.vjepa_demo import load_pretrained_vjepa_pt_weights, load_pretrained_vjepa_classifier_weights, build_pt_video_transform, device
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -8,11 +8,11 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 class VJEPAService:
     # From the demo code provided by Meta
 
-    def __init__(self, pt_model_path, classifier_model_path, img_size=384, num_frames=64):
+    def __init__(self, pt_model_path, classifier_model_path, img_size=256, num_frames=64):
         self.device = device
 
         print("Loading PyTorch VJEPA model...")
-        self.model_pt = vit_giant_xformers_rope(img_size=(img_size, img_size), num_frames=64)
+        self.model_pt = vit_large_rope(img_size=(img_size, img_size), num_frames=num_frames)
         self.model_pt.to(device).eval()
         load_pretrained_vjepa_pt_weights(self.model_pt, pt_model_path)
 
