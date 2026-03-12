544df8f4887289a1c5b92ef60e002643063f3e4b
Harman Aujla <harman11524@gmail.com>
2026-03-12 12:17:22 -0400

    Fix backend

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e  # exit on error
-
-if [ ! -d "vjepa2" ]; then
-  git clone --depth 1 https://github.com/facebookresearch/vjepa2.git vjepa2
-fi
-
-pip install -e ./vjepa2
-pip install -r requirements.txt
